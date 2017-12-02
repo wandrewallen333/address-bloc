cac514b261de9e493ecd51f81067fba509f436f4
Andrew Allen <w.andrewallen@gmail.com>
2017-12-02 10:36:19 -0600

    Assignment 4 AddressBloc Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,6 +8,18 @@
        @entries = []
      end
 
+     def remove_entry(name, phone_number, email)
+       delete_entry = nil
+
+       @entries.each do |entry|
+         if name == entry.name && phone_number == entry.phone_number && email == entry.email
+           delete_entry = entry
+         end
+       end
+
+       @entries.delete(delete_entry)
+     end
+
 
    def add_entry(name, phone_number, email)
      index = 0
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -28,13 +28,30 @@ RSpec.describe AddressBook do
 
     it "adds the correct information to entries" do
       book = AddressBook.new
-      
+
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
 
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "remove_entry" do
+    it "removes an  entry using the name, phone_number, and email address" do
+      book = AddressBook.new
+      book.add_entry('Andrew Allen', '314.595.0949', 'w.andrewallen@gmail.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email_address = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq 1
+      expect(book.entries.first.name).to eq('Andrew Allen')
     end
   end
 end
