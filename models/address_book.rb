45dd9a73382583ef7b94e19c2ed549a162634768
Andrew Allen <w.andrewallen@gmail.com>
2017-12-04 18:47:36 -0600

    Assignment 7 AddressBloc Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -62,4 +62,13 @@
      return nil
    end
 
+   def iterative_search(name)
+     @entries.each do |entry|
+       if entry.name == name
+         return entry
+       end
+     end
+      return nil  
+   end
+
  end
